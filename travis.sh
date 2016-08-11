1ca3cf98ba540fb55474ad386a31c3ff1453fd3b
dbmeneses <duarte.meneses@sonarsource.com>
2016-08-11 17:44:22 +0200

    Fix JAVA_HOME

--- travis.sh
+++ travis.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 set +x
 java -version
-echo "JAVA HOME: $JAVA_HOME"
-which java
-
+echo "JAVA_HOME: $JAVA_HOME"
+export JAVA_HOME=`dirname $(dirname $(which java))`
+echo "NEW JAVA_HOME: $JAVA_HOME"
 echo "Parameters in environment: $SONARQUBE_SCANNER_PARAMS"
 sonar-scanner -v
