509a0647ed2c49d8e1063ebd5940be0f71c3158e
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-12-05 16:05:31 +0100

    Test for build-wrapper

--- travis.sh
+++ travis.sh
@@ -7,5 +7,10 @@ echo "JAVA_HOME: $JAVA_HOME"
 export JAVA_HOME=`dirname $(dirname $(which java))`
 echo "NEW JAVA_HOME: $JAVA_HOME"
 echo "Parameters in environment: $SONARQUBE_SCANNER_PARAMS"
+
+make clean
+build-wrapper-linux-x86-64 --out-dir bw-output make all
+make test
+
 sonar-scanner -v
 
