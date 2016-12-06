e08834eaa29c74ec913f04ab629c2c8dfc0f5e91
dbmeneses <duarte.meneses@sonarsource.com>
2016-12-06 10:50:37 +0100

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -10,8 +10,4 @@ echo "NEW JAVA_HOME: $JAVA_HOME"
 echo "Parameters in environment: $SONARQUBE_SCANNER_PARAMS"
 sonar-scanner -v
 
-if [ -n "$(which build-wrapper-linux-x86-64)" ] ; then
-    build-wrapper-linux-x86-64 --out-dir bw-output make all
- fi
-
 echo "Done"
